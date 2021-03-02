9cad89aacfa89027c533d2c918d5ea04433f7ead
dzamin12 <63652666+dzamin12@users.noreply.github.com>
2021-03-02 11:08:45 -0500

    js signUp css checkpassword and email

---
+++ register/checkSignUp.js
@@ -0,0 +1,54 @@
+function checkSignUp() {
+    checkEmail();
+    checkPassword();
+
+    if(checkEmail() == true && checkPassword() == true)
+        return true;
+    else return false;
+}
+
+function checkEmail(){
+    const emailUp = document.getElementById("emailUp").value;
+    const confirmEmailUp = document.getElementById("confirmEmailUp").value;
+
+    if (emailUp != confirmEmailUp) {
+        visible("errorConfirmEmail", "emailFas");
+        return false;
+    }
+
+    else if(emailUp == confirmEmailUp){
+        hidden("errorConfirmEmail", "emailFas");
+        return true;
+    }
+}
+
+function checkPassword(){
+    const passwordUp = document.getElementById("passwordUp").value;
+    const confirmPasswordUp = document.getElementById("confirmPasswordUp").value;
+
+    if (passwordUp != confirmPasswordUp) {
+        visible("errorConfirmPassword", "passwordFas");
+        return false;
+    }
+
+    else if(passwordUp == confirmPasswordUp){
+        hidden("errorConfirmPassword", "passwordFas");
+        return true;
+    }
+}
+
+function visible(error, fas){
+    const small = document.getElementById(error);
+    const i = document.getElementById(fas);
+    small.style.visibility = "visible";
+    i.style.visibility = "visible";
+}
+
+function hidden(error, fas){
+    const small = document.getElementById(error);
+    const i = document.getElementById(fas);
+    small.style.visibility = "hidden";
+    i.style.visibility = "hidden";
+}
+
+
