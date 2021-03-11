c4dc0db2129885b741586e3f6350b6d978dde457
jaspreet305 <58188685+singharash09@users.noreply.github.com>
2021-03-11 09:28:23 -0500

    made contact us page and added some javascript

---
+++ backstore/backstore.js
@@ -0,0 +1,12 @@
+
+function deleteOrder(row){
+    row.closest('tr').remove();
+}
+
+function checkOrderMatch(order){
+    for (var i=0; i<$("#myTable").length; i++){
+        if ($("#myTable")[i] != order){
+            
+        }
+    }
+}
