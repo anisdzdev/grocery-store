9282debc00764fca1602b7c79019bc3e9eb7a640
jaspreet305 <58188685+singharash09@users.noreply.github.com>
2021-03-14 15:57:10 -0400

    h

--- assets/js/backstore.js
+++ assets/js/backstore.js
@@ -6,10 +6,16 @@ function deleteRow(row){
 }
 
 function checkOrderMatch(){
-    orderInput = $("order-number").val();
-    if (orderInput != orders[i]){
+    orderInput = $("#order-number").val();
+    console.log(orderInput)
 
-    } else if (orderInput == orders[i]){
-
+    for (i=0; i<orders.length; i++) {
+        if (orderInput != orders[i]) {
+            alert("Unknown order. Please check your order number and try again.")
+            break;
+        } else if (orderInput == orders[i]) {
+            $("#form-contact-body").text("Thank you for contacting us. Our team will be in touch as soon as possible!")
+            break;
+        }
     }
 }
