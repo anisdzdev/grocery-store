e48646bb14a304e339adaf973dacb58f201ca43a
jaspreet305 <58188685+singharash09@users.noreply.github.com>
2021-04-07 18:37:36 -0400

    sheesh 2

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbarx-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
             <div class="navbar-nav ">
