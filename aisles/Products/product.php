86723bdf7bb3fc1624a95c7e8c12a5c357b2447a
Anis Brachemi <anisbrachemi@hotmail.com>
2021-04-13 22:35:40 -0400

    Order-List

--- aisles/Products/product.php
+++ aisles/Products/product.php
@@ -35,10 +35,12 @@ if (($handle = fopen("../../backstore/database/products.csv", "r")) !== FALSE) {
     <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/uikit@3.6.15/dist/css/uikit.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <!--    <link rel="stylesheet" type="text/css"  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />-->
+<!--        <link rel="stylesheet" type="text/css"  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />-->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="../../assets/js/productsJQ.js"></script>
+
+
 
     <title>
         <?php
@@ -82,15 +84,6 @@ if (($handle = fopen("../../backstore/database/products.csv", "r")) !== FALSE) {
                 </div>
 
 
-
-
-
-
-
-
-
-
-
                     <?php
                         if($product[2] == $product[3]){
                             echo "<p class='product-price'>
@@ -122,15 +115,9 @@ if (($handle = fopen("../../backstore/database/products.csv", "r")) !== FALSE) {
 
 
 </div>
-<?php include ('../../recommended.php')?>
+<?php include('recommended.php') ?>
 
 <?php include ('../../footer.php')?>
-<!-- Scripts for the Bootstrap, ignore -->
-<script src="https://kit.fontawesome.com/45836f3eb4.js" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- aisles/Products/recommended.php
+++ aisles/Products/recommended.php
@@ -13,13 +13,13 @@
 
             <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
                 <div class="product card-aisle rounded uk-card-default card product">
-                    <a class="productLink" href="almond.php">
-                        <img class="productImage" alt="almonds" src="assets/images/articles/almond.png">
+                    <a class="productLink" href="snacks/almond.php">
+                        <img class="productImage" alt="almonds" src="../../assets/images/articles/Almond.png">
                     </a>
                     <div class="card-body productDetails">
                         <p class="productName">Almond</p>
                         <p class="productPrice"><span class="oldPrice">16.37/Kg</span><span class="deal"> $15.37/Kg</span></p>
-                        <a href="cart/cart.php">
+                        <a href="../../cart/cart.php">
                             <button name="submit" class="btn btn-primary" type="submit">Add to cart</button>
                         </a></div>
                 </div>
@@ -27,13 +27,13 @@
 
             <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
                 <div class="product card-aisle rounded  uk-card-default card product">
-                    <a class="productLink" href="../meat/chickenBreast.php">
-                        <img class="productImage" alt="Chicken Breast" src="assets/images/articles/chicken-breast.png">
+                    <a class="productLink" href="meat/chickenBreast.php">
+                        <img class="productImage" alt="Chicken Breast" src="../../assets/images/articles/chicken-breast.png">
                     </a>
                     <div class="card-body productDetails">
                         <p class="productName">Chicken Breast</p>
                         <p class="productPrice"><span class="oldPrice">21.99/Kg</span><span class="deal"> $19.99/Kg</span></p>
-                        <a href="../../../cart/cart.php">
+                        <a href="../../cart/cart.php">
                             <button name="submit" class="btn btn-primary" type="submit">Add to cart</button>
                         </a>
                     </div>
@@ -42,13 +42,13 @@
 
             <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
                 <div class="product card-aisle rounded  uk-card-default card product">
-                    <a class="productLink" href="../meat/chicken-wings.php">
-                        <img class="productImage" alt="Chicken Wings" src="assets/images/articles/chicken-wing.png">
+                    <a class="productLink" href="meat/chicken-wings.php">
+                        <img class="productImage" alt="Chicken Wings" src="../../assets/images/articles/chicken-wing.png">
                     </a>
                     <div class="card-body productDetails">
                         <p class="productName">Chicken Wings</p>
                         <p class="productPrice">7.99$/8pcs</p>
-                        <a href="cart/cart.php">
+                        <a href="../../cart/cart.php">
                             <button name="submit" class="btn btn-primary" type="submit">Add to cart</button>
                         </a>
                     </div>
@@ -57,13 +57,13 @@
 
             <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
                 <div class="product card-aisle rounded  uk-card-default card product">
-                    <a class="productLink" href="../fruitsAndVeg/oranges.php">
-                        <img class="productImage" alt="Oranges" src="assets/images/articles/oranges.png">
+                    <a class="productLink" href="fruitsAndVeg/oranges.php">
+                        <img class="productImage" alt="Oranges" src="../../assets/images/articles/Oranges.png">
                     </a>
                     <div class="card-body productDetails">
                         <p class="productName">Oranges</p>
                         <p class="productPrice">$1.50 each</p>
-                        <a href="cart/cart.php">
+                        <a href="../../cart/cart.php">
                             <button name="submit" class="btn btn-primary" type="submit">Add to cart</button>
                         </a>
                     </div>
